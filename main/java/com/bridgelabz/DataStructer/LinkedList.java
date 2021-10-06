dc72cab6b8cca0832b06e7c7888084830d531bca
aishwarya-stack <aishwaryaashokkadam@gmail.com>
2021-10-06 13:49:13 +0530

    ADD:welcome message

---
+++ main/java/com/bridgelabz/DataStructer/LinkedList.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.DataStructer;
+
+public class LinkedList 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to linkedlist program" );
+    }
+}
